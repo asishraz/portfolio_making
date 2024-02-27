da8d647b6795adefc2ddca2967bbbaaec35455ea
asishraz <iamasish3001@gmail.com>
2024-02-28 03:00:34 +0530

    form action changes

--- script/script.js
+++ script/script.js
@@ -1,8 +1,3 @@
-// local development only
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
 // for navbar readability 
 const header = document.querySelector('.navbar');
 window.onscroll = function() {
